4a3a014e2d2023bc0ad138010c1236614065c895
ChrisEsser <esser32@gmail.com>
2023-01-17 08:46:00 -0600

    forgot css versioning

--- app/layouts/views/admin.layout.php
+++ app/layouts/views/admin.layout.php
@@ -21,17 +21,17 @@ $action = $this->getVar('action');
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css"/>
     <link rel="stylesheet" href="https://unpkg.com/filepond/dist/filepond.css"/>
     <link rel="stylesheet" href="https://code.jquery.com/ui/1.13.2/themes/base/jquery-ui.css"/>
-    <link rel="stylesheet" href="/css/betterButtons.css?ver=99"/>
-    <link rel="stylesheet" href="/css/style.css?ver=99"/>
+    <link rel="stylesheet" href="/css/betterButtons.css?ver=100"/>
+    <link rel="stylesheet" href="/css/style.css?ver=100"/>
 
     <script src="https://code.jquery.com/jquery-3.6.0.min.js" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://unpkg.com/filepond-plugin-image-resize/dist/filepond-plugin-image-resize.js"></script>
     <script src="https://unpkg.com/filepond/dist/filepond.min.js"></script>
     <script src="https://unpkg.com/jquery-filepond/filepond.jquery.js"></script>
-    <script src="/js/confirm.js?ver=99"></script>
+    <script src="/js/confirm.js?ver=100"></script>
     <script src="/js/filepondHelper.js"></script>
-    <script src="/js/tableData.js?ver=99"></script>
+    <script src="/js/tableData.js?ver=100"></script>
     <?= HTML::displayHead() ?>
 </head>
 
